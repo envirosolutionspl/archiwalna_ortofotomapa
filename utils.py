415e4a5799ec495d719a1cf5a49527affa86f001
milenart <mikolaj.lenart17@gmail.com>
2025-12-17 14:24:27 +0100

    Dodanie pushLogInfo
    Dodano logi informujące o działaniu wtyczki

--- utils.py
+++ utils.py
@@ -1,5 +1,14 @@
 from .constants import QT_VER 
-
+from qgis.core import Qgis, QgsMessageLog
+from . import PLUGIN_NAME
 
 def isCompatibleQtVersion(cur_version, tar_version):
     return cur_version.startswith(QT_VER[tar_version])
+
+@staticmethod
+def pushLogInfo(message: str) -> None:
+    QgsMessageLog.logMessage(
+        message,
+        tag=PLUGIN_NAME,
+        level=Qgis.Info
+    )
