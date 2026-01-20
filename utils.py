cef15cdbdafb5edd1f49f9b7ee49edb684188f03
milenart <167006402+milenart@users.noreply.github.com>
2026-01-20 09:10:11 +0100

    Poprawki po testach #32 (#33)
    * Poprawki po testach
    
    Poprawiono funkcjonalności:
    Mapa jest automatycznie usuwana jeżeli już istniała przy uruchamianiu zamkniętej uprzednio wtyczce. Kontrolowana jest w ten sposób ilość warstw. Zawsze będzie tylko jedna.
    Przy uruchamianiu wtyczki, zakres automatycznie przybliżany jest do Warszawy.
    Poprawiono kompatybilność z Qt6 w testach.
    Wyrzucono nowe stałe do pliku constants.
    Zwiększono czas oczekiwania na odpowiedź z geoportalu na 2 minuty.
    Poprawiono GIF i README
    
    * Poprawa changelog
    
    * Poprawki do odniesień w PR
    
    * Poprawa utils

--- utils.py
+++ utils.py
@@ -2,13 +2,26 @@ from .constants import QT_VER
 from qgis.core import Qgis, QgsMessageLog
 from . import PLUGIN_NAME
 
-def isCompatibleQtVersion(cur_version, tar_version):
-    return cur_version.startswith(QT_VER[tar_version])
+class QtUtils:
+    
+    @staticmethod
+    def isCompatibleQtVersion(cur_version, tar_version):
+        return cur_version.startswith(QT_VER[tar_version])
 
-@staticmethod
-def pushLogInfo(message: str) -> None:
-    QgsMessageLog.logMessage(
-        message,
-        tag=PLUGIN_NAME,
-        level=Qgis.Info
-    )
+class MessageUtils:
+
+    @staticmethod
+    def pushLogInfo(message: str) -> None:
+        QgsMessageLog.logMessage(
+            message,
+            tag=PLUGIN_NAME,
+            level=Qgis.Info
+        )
+
+    @staticmethod
+    def pushLogWarning(message: str) -> None:
+        QgsMessageLog.logMessage(
+            message,
+            tag=PLUGIN_NAME,
+            level=Qgis.Warning
+        )
